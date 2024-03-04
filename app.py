807a84d22e2d751b457eda6ea89a061d72b36d4b
Aarhuu <aarni.huuskonen95@gmail.com>
2024-03-04 23:12:51 +0200

    removed other stuff

--- app.py
+++ app.py
@@ -1,22 +1,123 @@
+
+from datetime import date
+
 import dash
-from dash import Dash, html, dcc
+from dash import html, dcc, callback, Dash
+from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
+import plotly.graph_objects as go
+from dash.dependencies import Input, Output, State
+
+from utils import *
 
 
+try:
+    tickers = get_tickers()
+except Exception as e:
+    print(e)
 
-app = Dash(__name__, use_pages=True)
+app = Dash(__name__, use_pages=False, external_stylesheets=[dbc.themes.LUX])
 
 server = app.server
-
 app.layout = html.Div([
-    html.H1('Multi-page app with Dash Pages'),
+    html.H1("Portfolio sharpe ratio optimizer for S&P500 stocks"),
+    dbc.Label("Select at least two stocks from the dropdown menu", html_for="dropdown"),
     html.Div([
-        html.Div(
-            dcc.Link(f"{page['name']}", href=page["relative_path"])
-        ) for page in dash.page_registry.values()
+        dcc.Dropdown(id="ticker_dropdown", options=tickers.Security.values, value="", multi=True)
+    ],
+    style={"width": "30%"}),
+    dbc.Label("Select a time frame for calculating average daily returns and volatility (risk)", html_for="datepicker"),
+    html.Br(),
+    dcc.DatePickerRange(
+        id="datepicker",
+        min_date_allowed=date(2000, 1, 1),
+        max_date_allowed=date.today(),
+        start_date_placeholder_text="Start Period",
+        end_date_placeholder_text="End Period",
+        initial_visible_month=date.today(),
+    ),
+    html.Br(),
+    html.Div([
+    dbc.Button("Plot prices, average daily returns and volatility", id="plot_data", n_clicks=0),
+    ],
+    className="me-1 mt-1 btn btn-primary", style={"width": "40vh"}),
+    html.Br(),
+    html.Div([
+    dbc.Button("Optimize portfolio weights", id="optimize_portfolio", n_clicks=0),
+    ],
+    className="me-1 mt-1 btn btn-primary", style={"width": "40vh"}),
+    html.Div([
+        html.Div(id="stock_prices", style={"display": "inline-block", "width": "50%"}),
+        html.Div(id="returns_volas", style={"display": "inline-block", "width": "50%"})
     ]),
-    dash.page_container
+    html.Div([
+        html.Div(id="optimizer", style={"display": "inline-block", "width": "80%"})
+    ])
 ])
 
+@callback(
+    Output("stock_prices", "children"),
+    Output("returns_volas", "children"),
+    Input("plot_data", "n_clicks"),
+    [State("ticker_dropdown", "value"),
+     State("datepicker", "start_date"),
+     State("datepicker", "end_date")]
+)
+
+def plot_data(n_clicks, selected_stocks, start_date, end_date):
+    if not start_date or not end_date or len(selected_stocks) < 2:
+        raise PreventUpdate
+    symbols = tickers[tickers["Security"].isin(selected_stocks)]["Symbol"]
+    port = Portfolio(list(symbols), start_date, end_date)
+    port.download_close_data()
+    port.get_daily_returns()
+    port.get_avg_daily_returns()
+    port.get_volatility()
+    fig1 = go.Figure()
+    for sym in symbols:
+        fig1.add_trace(go.Scatter(mode="markers", x=[port.volatility[sym]], y=[port.avg_daily_returns[sym]*100], name=tickers[tickers["Symbol"] == sym]["Security"].values[0], 
+                                  marker=dict(size=15, line=dict(width=2))
+                                  )
+                                  )
+    fig1.update_layout(xaxis_title="Volatility", yaxis_title="Average of daily returns")
+
+    fig2 = go.Figure()
+    for sym in symbols:
+       fig2.add_trace(go.Line(x=port.close_data.index.values, y=port.close_data[sym].values, name=tickers[tickers["Symbol"] == sym]["Security"].values[0]))
+
+    fig2.update_layout(xaxis_title="Date", yaxis_title="Closing price $")
+
+    return dcc.Graph(figure=fig2), dcc.Graph(figure=fig1)
+
+@callback(
+    Output("optimizer", "children"),
+    Input("optimize_portfolio", "n_clicks"),
+    [State("ticker_dropdown", "value"),
+     State("datepicker", "start_date"),
+     State("datepicker", "end_date")]
+)
+
+def optimise_portfolio(n_clicks, selected_stocks, start_date, end_date):
+    if not start_date or not end_date or len(selected_stocks) < 2:
+        raise PreventUpdate
+    symbols = tickers[tickers["Security"].isin(selected_stocks)]["Symbol"]
+    port = Portfolio(list(symbols), start_date, end_date)
+    port.download_close_data()
+    port.get_daily_returns()
+    port.get_avg_daily_returns()
+    port.get_volatility()
+    port.optimize()
+    port.get_optimization_results()
+    names = []
+    for sym in port.series.index:
+        names.append(tickers[tickers["Symbol"] == sym]["Security"].values[0])
+    if port.results.success:
+        fig = go.Figure([go.Bar(x=names, y=port.series.values)])
+        fig.update_layout(xaxis_title="Stock symbol", yaxis_title="Weight")
+        return [f"Optimal allocation found with Sharpe ratio of {np.round(port.optim_sharpe, decimals=3)}. Extrapolated annual return for the portfolio is {np.round(port.optim_returns, decimals=3)}% with volatility of {np.round(port.optim_vola, decimals=3)}", dcc.Graph(figure=fig)]
+    else:
+        return "Optimal allocation not found. Select other stocks and/or timeframe"
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 import yfinance as yf
 import numpy as np
 import scipy
-from scipy.optimize import minimize
+from scipy.optimize import minimize, LinearConstraint
 
 def get_tickers():
     return pd.read_html(
@@ -54,10 +54,10 @@ class Portfolio():
 
     def optimize(self) -> None:
 
-        def portfolio_returns(w):
-            #annualized_returns = np.dot(np.transpose(weights), self.avg_daily_returns)*252
-            annualized_returns = np.sqrt(np.dot(w, np.dot(w, self.covariance)))/(self.avg_daily_returns*252).dot(w)
-            return annualized_returns
+        def portfolio_reverse_returns(w):
+            optim = np.dot(w, self.avg_daily_returns)/(np.sqrt(np.dot(w, np.dot(self.covariance, w))*252))
+            
+            return -optim
         
         weights = [1/len(self.tickers)]*len(self.tickers)
         selections = tuple((0, 1) for i in range(len(self.tickers)))
@@ -66,176 +66,19 @@ class Portfolio():
         )
  
         self.results = minimize(
-            fun=portfolio_returns,
+            fun=portfolio_reverse_returns,
             x0=weights,
+            method='SLSQP',
             bounds=selections,
             constraints=constraints
             )
         
     def get_optimization_results(self):
         min_w = self.results.x
-        self.optim_returns = self.avg_daily_returns.dot(min_w)
-        print(min_w)
-        print(self.avg_daily_returns)
-        self.optim_vola = np.sqrt(np.dot(min_w, np.dot(min_w, self.covariance)))
-        print(self.volatility)
-        print(self.covariance)
-        print(self.optim_vola)
-
-    
-"""
-class EnergySystem
-
-    - panel area of local solar power plant in square meters
-    - daily_avg_energy use in kWh
-    - storage capacity in kWh
-    - max storage_duration in hours (e.g. 48 hours equals two days)
-    - spot_data from Oomi energia website
-
-"""
-
-# per day average per month MJ/m2 in Finland
-daily_avg_solar = {
-	"1": 32/31,
-	"2": 90/28,
-	"3": 244/31,
-	"4": 403/30,	
-	"5": 592/31,	
-	"6": 615/30,	
-	"7": 615/31,	
-	"8": 470/31,	
-	"9": 270/30,	
-	"10": 120/31,	
-	"11": 35/30,	
-	"12": 16/31,	
-}
-
-class EnergySystem():
-    def __init__(self, 
-                 panel_area, 
-                 grid_cost,
-                 daily_avg_energy_use, 
-                 storage_capacity,
-                 max_storage_duration = 2,
-                 full_cap_discharge = 0.1) -> None:
-        self.panel_area: float = panel_area
-        self.grid_cost: float = grid_cost
-        self.avg_energy_use: float = daily_avg_energy_use
-        self.storage_capacity: float = storage_capacity
-        self.max_storage_duration: float = max_storage_duration
-        self.full_cap_discharge: float = full_cap_discharge
-        self.spot_data: pd.DataFrame = pd.DataFrame()
-        self.optim_h_bands: list = [0.0]
-        self.optim_l_bands: list = [0.0]
-        self.panel_efficiency: float = 0.8
-        self.solar_to_electricity: float = 0.27777
-        self.electricity_tax: float = 2.253
-        self.simulated_savings: list = [0.0]
-        self.simulated_storage: list = [0.0]
-    
-    def get_spot_data(self, filepath):
-        self.spot_data = pd.read_csv(filepath)
-        self.spot_data["Aika"] = pd.to_datetime(self.spot_data["Aika"], format="%Y-%m-%d %H:%M")
-    
-    def simulate_system(self, step_size):
-        sunlight_hours = range(10, 17)
-
-        def get_action(initial_storage, hour):
-
-            if (self.spot_data.iloc[-1, 0] - self.spot_data.iloc[hour, 0]).days < 1:
-                day_ahead_data = self.spot_data.iloc[hour:,:]
-            else:
-                day_ahead_data = self.spot_data.iloc[hour:hour+24,:]  
-
-            l_band = day_ahead_data["c/kWh"].min()
-            h_band = day_ahead_data["c/kWh"].max()      
-            best_result = 0.0
-
-            optim_h_band = h_band
-            optim_l_band = l_band
-
-            while h_band >= day_ahead_data["c/kWh"].min():
-                while l_band <= h_band:
-                    savings = [0.0]
-                    storage = [initial_storage]
-                    for h in range(0, day_ahead_data.shape[0]):
-                        month = day_ahead_data.iloc[h, 0].month
-                        hour_of_day = day_ahead_data.iloc[h, 0].hour
-                        price = day_ahead_data.iloc[h, 1]
-                        if price < l_band and storage[h-1] < self.storage_capacity and hour_of_day in sunlight_hours:
-                            available_solar = daily_avg_solar[str(month)]/len(sunlight_hours)*self.panel_area*self.panel_efficiency*self.solar_to_electricity
-                            if available_solar < self.storage_capacity - storage[h-1]:
-                                storage.append(storage[h-1] + available_solar)
-                                savings.append(savings[h-1] + (self.avg_energy_use/24)*(price + self.grid_cost + self.electricity_tax)/100)
-
-                            else:
-                                storage.append(self.storage_capacity)
-                                savings.append(savings[h-1] + (self.avg_energy_use/24 - (storage[h] - storage[h-1]))*(price + self.grid_cost + self.electricity_tax)/100)
-
-                        elif price > h_band and storage[h-1] > 0.0:
-                            used_storage_energy = np.min([self.avg_energy_use/24.0, storage[h-1]])
-                            storage.append(storage[h-1] - used_storage_energy)
-                            savings.append(savings[h-1] - used_storage_energy*(price + self.grid_cost + self.electricity_tax)/100)
-
-                        elif storage[h-1] == self.storage_capacity:
-                            used_storage_energy = np.min([self.avg_energy_use/24.0, storage[h-1]*self.full_cap_discharge])
-                            storage.append(storage[h-1] - used_storage_energy)
-                            savings.append(savings[h-1] - used_storage_energy*(price + self.grid_cost + self.electricity_tax)/100)
-                        else:
-                            storage.append(storage[h-1]*0.99)
-                            savings.append(savings[h-1])
-                    
-                    if -1*savings[-1] > best_result: 
-                        best_result = -1*savings[-1]
-                        optim_h_band = h_band
-                        optim_l_band = l_band
-                    l_band += step_size
-                l_band = day_ahead_data["c/kWh"].min()
-                h_band -= step_size
-            return optim_h_band, optim_l_band
-
-      
-
-        for hour in range(1, len(self.spot_data)):
-            
-            optim_h_band, optim_l_band = get_action(self.simulated_storage[hour-1], hour)
-            self.optim_h_bands.append(optim_h_band)
-            self.optim_l_bands.append(optim_l_band)
-            hour_of_day = self.spot_data.iloc[hour, 0].hour
-            month = self.spot_data.iloc[hour, 0].month
-            price = self.spot_data.iloc[hour, 1]
-            if self.spot_data.iloc[hour, 1] < optim_l_band and self.simulated_storage[hour-1] < self.storage_capacity and hour_of_day in sunlight_hours:
-                available_solar = daily_avg_solar[str(month)]/len(sunlight_hours)*self.panel_area*self.panel_efficiency*self.solar_to_electricity
-                if available_solar < self.storage_capacity - self.simulated_storage[hour-1]:
-                    self.simulated_storage.append(self.simulated_storage[hour-1] + available_solar)
-                    self.simulated_savings.append(self.simulated_savings[hour-1] + (self.avg_energy_use/24)*(price + self.grid_cost + self.electricity_tax)/100)
-
-                else:
-                    self.simulated_storage.append(self.storage_capacity)
-                    self.simulated_savings.append(self.simulated_savings[hour-1] + (self.avg_energy_use/24 - (self.simulated_storage[hour] - self.simulated_storage[hour-1]))*(price + self.grid_cost + self.electricity_tax)/100)
-
-            elif price > optim_h_band and self.simulated_storage[hour-1] > 0.0:
-                used_storage_energy = np.min([self.avg_energy_use/24.0, self.simulated_storage[hour-1]])
-                self.simulated_storage.append(self.simulated_storage[hour-1] - used_storage_energy)
-                self.simulated_savings.append(self.simulated_savings[hour-1] - used_storage_energy*(price + self.grid_cost + self.electricity_tax)/100)
-
-            elif self.simulated_storage[hour-1] == self.storage_capacity:
-                used_storage_energy = np.min([self.avg_energy_use/24.0, self.simulated_storage[hour-1]*self.full_cap_discharge])
-                self.simulated_storage.append(self.simulated_storage[hour-1] - used_storage_energy)
-                self.simulated_savings.append(self.simulated_savings[hour-1] - used_storage_energy*(price + self.grid_cost + self.electricity_tax)/100)
-            else:
-                self.simulated_storage.append(self.simulated_storage[hour-1]*0.99)
-                self.simulated_savings.append(self.simulated_savings[hour-1])
-
-if __name__ == "__main__":
-    energy_system = EnergySystem(
-        panel_area=50, 
-        grid_cost= 5.0,
-        daily_avg_energy_use=30, 
-        storage_capacity=20)
-    energy_system.get_spot_data("./Oomi Spot-hintatieto.csv")
-    energy_system.simulate_system(step_size=3.5)
-    print(energy_system.simulated_storage)
+        self.series = pd.Series(min_w, index=self.avg_daily_returns.index)
+        self.optim_sharpe = np.dot(min_w, self.avg_daily_returns)/np.sqrt(np.dot(min_w, np.dot(min_w, self.covariance))*252)*100
+        self.optim_returns = np.dot(min_w, self.avg_daily_returns)*100
+        self.optim_vola = np.sqrt(np.dot(min_w, np.dot(self.covariance,min_w))*252)
 
     
     
